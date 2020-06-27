01668172a1bc1983c2054f547ce314b90876f30b
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-06-27 03:01:12 -0300

    wip, maybe deprecated

--- src/services/api.js
+++ src/services/api.js
@@ -1,35 +1,23 @@
-'use strict'
+// 'use strict'
 
-const axios = require('axios')
+// const Twit = require('twit')
 
-const api = axios.create({
-  // baseURL: 'https://api.twitter.com/1.1/statuses/',
-  // consumer-secret="${process.env.CONSUMER_SECRET}",
-  // 'token-secret': "${process.env.TOKEN_SECRET}",
+// const twit = new Twit({
+//   consumer_key: process.env.CONSUMER_KEY,
+//   consumer_secret: process.env.CONSUMER_SECRET,
+//   access_token: process.env.TOKEN,
+//   access_token_secret: process.env.TOKEN_SECRET,
+//   timeout_ms: 10000
+// })
 
-  timeout: 1000,
-  headers: {
-    authorization: `OAuth
-    oauth_consumer_key="${process.env.CONSUMER_KEY}",
-    oauth_nonce="generated_oauth_nonce",
-    oauth_signature="generated_oauth_signature",
-    oauth_signature_method="HMAC-SHA1",
-    oauth_timestamp="generated_timestamp",
-    oauth_token="${process.env.TOKEN}",
-    oauth_version="1.0"`
-  }
-})
-
-function getTimeline() {
-  return api
-    .get('https://api.twitter.com/1.1/statuses/home_timeline.json')
-    .then(result => result.data)
-}
-
-function getUserTimeline(username = '') {
-  return api
-    .get('user_timeline.json')
-    .then(result => result.data)
-}
-
-module.exports = { getTimeline, getUserTimeline }
+// export default async function getTimeline() {
+//   return twit.get('statuses/user_timeline',
+//     { screen_name: 'devmozao' },
+//     function (error, data, response) {
+//       if (error) {
+//         console.log(error)
+//         return error
+//       }
+//       return data
+//     })
+// }
