e2ca98d0342746cb62e3202a4b29c53b7e836814
Diogo Mozão Fonseca <devmozao@outlook.com>
2020-08-01 02:21:41 -0300

    fixed identation

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ const PORT = process.env.PORT
 const routes = require('./routes')
 
 app.disable('x-powered-by')
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
 // routes
 app.use(routes)
